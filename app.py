d6a390522d5e30ad6bbdfeb67198de26de347de7
usmanhasan123 <usmanh_22@hotmail.com>
2025-07-20 13:51:18 -0700

    fixed syntax error

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ def add_input():
 
 press=st.button("Click here if you want to put more options: ", on_click=add_inputs)
 for i in st.session_state.inputs:
-    no_of_patty_gone=st.text_inputs("no of patty gone: ", key=f"patty_{i}")
-    egg_type=st.text_inputs("egg type: ", key=f"type_{i}")
-    rate=st.text_inputs("rate: ", key=f"rate_{i}")
-    cut=st.text_inputs("cut: ", key=f"cut_{i}")
-    open_or_closed=st.text_inputs("open or closed: ", key=f"open_{i}")
-    party=st.text_inputs("party: ", key=f"party_{i}")
+    no_of_patty_gone=st.text_input("no of patty gone: ", key=f"patty_{i}")
+    egg_type=st.text_input("egg type: ", key=f"type_{i}")
+    rate=st.text_input("rate: ", key=f"rate_{i}")
+    cut=st.text_input("cut: ", key=f"cut_{i}")
+    open_or_closed=st.text_input("open or closed: ", key=f"open_{i}")
+    party=st.text_input("party: ", key=f"party_{i}")
     dictt[f"{i}st party"]={"no_of_patty_gone": no_of_patty_gone, "egg_type": egg_type, "rate":rate, "cut":cut, "open_or_closed": open_or_closed, "party":party}
 
 
