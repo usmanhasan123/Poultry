406878ed6e7b369640bbf6f4000634ac60c072c3
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-21 13:18:20 -0700

    Update get_and_upload_daily_production.py

--- get_and_upload_daily_production.py
+++ get_and_upload_daily_production.py
@@ -23,7 +23,7 @@ class production_class:
         # 'Trusted_Connection=yes;'
         # )
         password=password=quote_plus('Oxygen123$')
-        conn=mysql.connector.connect(host='127.0.0.1', port=3307, user='user', password=password, database='poultry_db')
+        # conn=mysql.connector.connect(host='127.0.0.1', port=3307, user='user', password=password, database='poultry_db')
         conn = create_engine(f"mysql+mysqlconnector://user:{password}@127.0.0.1:3307/poultry_db")
 
         return conn
