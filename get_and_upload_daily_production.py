efa44c03b53c897930b6a58525acf37f7a09db74
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-21 13:15:50 -0700

    Update get_and_upload_daily_production.py

--- get_and_upload_daily_production.py
+++ get_and_upload_daily_production.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 import pyodbc
 import mysql.connector
 from urllib.parse import quote_plus
+from sqlalchemy import create_engine
 
 class production_class:
     def __init__(self, inputs):
@@ -23,6 +24,8 @@ class production_class:
         # )
         password=password=quote_plus('Oxygen123$')
         conn=mysql.connector.connect(host='127.0.0.1', port=3307, user='user', password=password, database='poultry_db')
+        conn = create_engine(f"mysql+mysqlconnector://user:{password}@127.0.0.1:3307/poultry_db")
+
         return conn
     def insert_in_daily_report(self): # need to replace if date is repeated
         conn=self.create_connection()
