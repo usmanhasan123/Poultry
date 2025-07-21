4bbbfe81c2752b3e3aae1616648f88c37bc1514d
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-21 08:54:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import datetime
 
-dictt={}
+input_dict={}
 st.set_page_config(page_title="Daily Production Stats", page_icon=":robot:")
 st.header("Production Stats")
 date=st.date_input("date: ",value= datetime.date.today(), key="date_input")
-
+remaining_balance_big_eggs=st.number_input("Remaining balance for large eggs: ", key=rem_large_key)
+remaining_balance_small_eggs=st.number_input("Remaining balance for small eggs: ", key=rem_small_key)
+input_dict['date']=date
+input_dict['remaining_balance_big_eggs']=remaining_balance_big_eggs
+input_dict['remaining_balance_small_eggs']=remaining_balance_small_eggs
 if 'inputs' not in st.session_state:
     st.session_state.inputs=1
     
@@ -24,9 +28,9 @@ for i in range(st.session_state.inputs):
     cut=cols[3].number_input("cut: ", key=f"cut_{i}")
     open_or_closed=cols[4].selectbox("open or closed: ", ['open', 'closed'], key=f"open_{i}")
     party=cols[5].selectbox("party: ", ['Siddiq', 'Zulfi'], key=f"party_{i}")
-    dictt[f"{i+1}st party"]={"no_of_patty_gone": no_of_patty_gone, "egg_type": egg_type, "rate":rate, "cut":cut, "open_or_closed": open_or_closed, "party":party}
+    input_dict[f"{i+1}st party"]={"no_of_patty_gone": no_of_patty_gone, "egg_type": egg_type, "rate":rate, "cut":cut, "open_or_closed": open_or_closed, "party":party}
 
 if st.button("Submit"):
-    st.write(dictt)
+    st.write(input_dict)
 
     
