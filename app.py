e6c200267718814b6985776cc8062fb3560541e6
usmanhasan123 <usmanh_22@hotmail.com>
2025-07-21 01:08:31 -0700

    update app.py

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ def add_inputs():
 
 press=st.button("Click here if you want to put more options: ", on_click=add_inputs)
 for i in range(st.session_state.inputs):
-    no_of_patty_gone=st.text_input("no of patty gone: ", key=f"patty_{i}")
-    egg_type=st.text_input("egg type: ", key=f"type_{i}")
-    rate=st.text_input("rate: ", key=f"rate_{i}")
-    cut=st.text_input("cut: ", key=f"cut_{i}")
-    open_or_closed=st.text_input("open or closed: ", key=f"open_{i}")
-    party=st.text_input("party: ", key=f"party_{i}")
+    cols=st.columns(6)
+    no_of_patty_gone=cols[0].text_input("no of patty gone: ", key=f"patty_{i}")
+    egg_type=cols[0].text_input("egg type: ", key=f"type_{i}")
+    rate=cols[0].text_input("rate: ", key=f"rate_{i}")
+    cut=cols[0].text_input("cut: ", key=f"cut_{i}")
+    open_or_closed=cols[0].text_input("open or closed: ", key=f"open_{i}")
+    party=cols[0].text_input("party: ", key=f"party_{i}")
     dictt[f"{i}st party"]={"no_of_patty_gone": no_of_patty_gone, "egg_type": egg_type, "rate":rate, "cut":cut, "open_or_closed": open_or_closed, "party":party}
 
 
