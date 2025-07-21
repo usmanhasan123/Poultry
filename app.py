a9b5f0ca2a00f46cf5ff0d6d6beb7a7b10b3c317
usmanhasan123 <usmanh_22@hotmail.com>
2025-07-21 00:54:49 -0700

    fixed syntax error

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def add_inputs():
     st.session_state.inputs=st.session_state.inputs+1
 
 press=st.button("Click here if you want to put more options: ", on_click=add_inputs)
-for i in st.session_state.inputs:
+for i in range(st.session_state.inputs):
     no_of_patty_gone=st.text_input("no of patty gone: ", key=f"patty_{i}")
     egg_type=st.text_input("egg type: ", key=f"type_{i}")
     rate=st.text_input("rate: ", key=f"rate_{i}")
