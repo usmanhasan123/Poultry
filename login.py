d50007ad9cc5cd73085f6b9422f3d04ed365fff3
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-23 11:48:26 -0700

    Update login.py

--- login.py
+++ login.py
@@ -12,15 +12,15 @@ def login(username, password):
     if (user==username) and (pswd==password):
       st.session_state['logged_in']=True
       st.success("Logged in successfully")
-      st.write('username and password corrcet')
+      # st.write('username and password corrcet')
     else:
       st.error("username or password incorrect")
       
-# if 'logged_in' not in st.session_state:
-st.session_state['logged_in']=False
+if 'logged_in' not in st.session_state:
+  st.session_state['logged_in']=False
   
 if st.session_state['logged_in']==False:
   login(username, password)
-# else:
-#   st.write('username and password corrcet')
+else:
+  st.write('username and password corrcet')
   # subprocess.run(['python', 'app.py'])
