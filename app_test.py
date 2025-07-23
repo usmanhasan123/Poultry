34b58d3ecb8185966f0a9502395d51340042a63c
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-23 06:21:34 -0700

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,1 +1,6 @@
 
+
+
+password=password=quote_plus('Oxygen123$')
+# conn=mysql.connector.connect(host='127.0.0.1', port=3307, user='user', password=password, database='poultry_db')
+conn = create_engine(f"mysql+mysqlconnector://user:{password}@localhost:3307/poultry_db")
