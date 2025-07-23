935301037d6369bc9c6f5bd0a195bc72c4ab2345
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-23 06:22:07 -0700

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,5 +1,8 @@
-
-
+import os
+os.chdir('./')
+import streamlit as st
+import datetime
+from get_and_upload_daily_production import production_class
 
 password=password=quote_plus('Oxygen123$')
 # conn=mysql.connector.connect(host='127.0.0.1', port=3307, user='user', password=password, database='poultry_db')
