b11abbd35a0d39f557ba77406d3acf7288b96dc7
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-23 11:35:20 -0700

    Update and rename login to login.py

---
+++ login.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import subprocess
+
+username = "root"
+password = "Oxygen123$"
+
+def login(username, password):
+  user= st.text_input("username: ", key='user_key')
+  pswd= st.text_input("password: ", key='pass_key')
+
+  if st.button("Login"):
+    if (user=username) && (pwsd=password):
+      st.session_state['logged_in']=True
+      st.success("Logged in successfully")
+    else:
+      st.error("username or password incorrect")
+      
+if 'logged_in' not in st.session_state:
+  st.session_state['logged_in']=False
+  
+if st.session_state['logged_in']==False:
+  login(username, password)
+else:
+  st.write('username and password corrcet')
+  # subprocess.run(['python', 'app.py'])
