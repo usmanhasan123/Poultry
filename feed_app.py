6355f69632583b955b6303cfd72f57ab45848a59
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-24 06:18:07 -0700

    Update feed_app.py

--- feed_app.py
+++ feed_app.py
@@ -2,37 +2,31 @@ import os
 os.chdir('./')
 import streamlit as st
 import datetime
-from get_and_upload_daily_production import production_class
+from get_and_upload_daily_production import production_class # import feed class
 
 def main():
     input_dict={}
-    st.set_page_config(page_title="Daily Production Stats", page_icon=":robot:")
-    st.header("Production Stats")
+    st.set_page_config(page_title="Feed Logs", page_icon=":robot:")
+    st.header("Feed Logs")
     date=st.date_input("date: ",value= datetime.date.today(), key="date_input")
-    remaining_balance_big_eggs=st.number_input("Remaining balance for large eggs: ", key='rem_large_key')
-    remaining_balance_small_eggs=st.number_input("Remaining balance for small eggs: ", key='rem_small_key')
     input_dict['date']=date
-    input_dict['remaining_balance_big_eggs']=remaining_balance_big_eggs
-    input_dict['remaining_balance_small_eggs']=remaining_balance_small_eggs
-    if 'inputs' not in st.session_state:
-        st.session_state.inputs=1
+    if 'inputs_2' not in st.session_state:
+        st.session_state.inputs_2=1
         
     def add_inputs():
-        st.session_state.inputs+=1
+        st.session_state.inputs_2+=1
     def remove_inputs():
-        st.session_state.inputs-=1
+        st.session_state.inputs_2-=1
     
     press=st.button("Add option", on_click=add_inputs)
     press1=st.button("Remove option", on_click=remove_inputs)
-    for i in range(st.session_state.inputs):
-        cols=st.columns(6)
-        no_of_patty_gone=cols[0].number_input("no of patty gone: ", key=f"patty_{i}")
-        egg_type=cols[1].selectbox("egg type: ", ['big', 'small'], key=f"type_{i}")
-        rate=cols[2].number_input("rate: ", key=f"rate_{i}")
-        cut=cols[3].number_input("cut: ", key=f"cut_{i}")
-        open_or_closed=cols[4].selectbox("open or closed: ", ['open', 'closed'], key=f"open_{i}")
-        party=cols[5].selectbox("party: ", ['Siddiq', 'Zulfi'], key=f"party_{i}")
-        input_dict[f"{i+1}st party"]={"no_of_patty_gone": no_of_patty_gone, "egg_type": egg_type, "rate":rate, "cut":cut, "open_or_closed": open_or_closed, "party":party}
+    for i in range(st.session_state.inputs_2):
+        cols=st.columns(4)
+        bori_amount=cols[0].number_input("amount (bori): ", key=f"bori_{i}")
+        bilti_payment=cols[1].number_input("Bilti payment: ", key=f"bilti_{i}")
+        paid_by=cols[1].selectbox("Paid by: ", ['Shahid', 'Siddiq'], key=f"paidby_{i}")
+        dispatch_receipt=cols[3].number_input("dispatch receipt: ", key=f"dispatch_{i}")
+        input_dict[f"{i+1}st feed"]={"bori_amount": bori_amount, "bilti_payment": bilti_payment, "paid_by":paid_by, "dispatch_receipt":dispatch_receipt}
     
     if st.button("Submit"):
         if len(input_dict)>0:
