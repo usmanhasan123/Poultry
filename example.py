7506499a621de6373cbadf64256caee4e3f7a877
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-25 13:29:11 -0700

    Create example.py

---
+++ example.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+if 'test' not in st.sesison_state:
+  st.session_state.test=0
+  
+if st.button("Submit"):
+  st.session_state.test=1
+
+if st.session_state.test==1:
+  st.write("this is a test case")
