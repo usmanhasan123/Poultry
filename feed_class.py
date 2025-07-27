f56b41b176e582455a463901bd44cdac77f6002d
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-27 10:17:07 -0700

    Update feed_class.py

--- feed_class.py
+++ feed_class.py
@@ -132,7 +132,17 @@ class feed_class:
             con.execute(text(query), recs)
             con.commit()
 
-    # def update_feed_tabel_master(self)
+    def update_feed_tabel_master(self):
+        conn=self.create_connection()
+        columns_to_update=self.inputs.keys()
+        set_clause = ', '.join(f"{i}=:{i}" for i in columns_to_update if i != 'id')
+        query = f"update feed_log_master set {set_clause} where id=:id"
+        recs=self.inputs
+        # recs=[{'1': self.inputs['arrival_date'], '2': self.inputs['arrival_receipt'], '3': 'ARRIVED', '4': self.inputs['car_number']}]
+
+        with conn.connect() as con:
+            con.execute(text(query), recs)
+            con.commit()
             
     @staticmethod
     def fetch_feed_log():
