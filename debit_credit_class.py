bd7fe29bdebb3308a8c79aeb461abb9466e3942a
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-29 10:19:33 -0700

    Update debit_credit_class.py

--- debit_credit_class.py
+++ debit_credit_class.py
@@ -21,7 +21,17 @@ class debit_credit:
 
     def insert_debit(self):
         conn=self.create_connection()
-        dff=production_class.fetch_daily_report()
+        dff=self.fetch_debit_credit_log()
+        max_id=dff['id'].max()
+        
+        if 'int' in str(type(max_id)):
+            max_id=int(max_id) # so that it is compatible with mysql
+            max_id=max_id+1
+        else:
+            max_id=19
+            max_id=int(max_car_no)
+        
+        
         final_input={}
         party_count={}
         input_keys=[]
@@ -36,14 +46,14 @@ class debit_credit:
                 final_input['week'] = int(pd.to_datetime(self.inputs['date']).strftime("%W"))
                 final_input['credit_amount'] = 0
                 
-                if final_input['party'] not in party_count:
-                    party_count[final_input['party']] = 0
-                else:
-                    party_count[final_input['party']]=party_count[final_input['party']]+1
-
-                ids=dff[(dff['date']==self.inputs['date']) & (dff['party']==self.inputs[i]['party'])]['id'].to_list()
-
-                final_input['report_id'] = ids[party_count[final_input['party']]]
+                final_input['report_id'] = max_id
+                max_id=max_id+1
+                # if final_input['party'] not in party_count:
+                #     party_count[final_input['party']] = 0
+                # else:
+                #     party_count[final_input['party']]=party_count[final_input['party']]+1
+                # ids=dff[(dff['date']==self.inputs['date']) & (dff['party']==self.inputs[i]['party'])]['id'].to_list()
+                # final_input['report_id'] = ids[party_count[final_input['party']]]
     
                 query = "insert into debit_credit_table (debit_date, debit_description, debit_amount, party, week, credit_amount, report_id) \
                 values (:debit_date, :debit_description, :debit_amount, :party, :week, :credit_amount, :report_id)"
