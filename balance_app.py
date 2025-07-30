263cd1bb93056b071e1a218ae7394f13ed39505f
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-07-30 11:04:09 -0700

    Update balance_app.py

--- balance_app.py
+++ balance_app.py
@@ -1,1 +1,19 @@
 
+import os
+os.chdir('./')
+import streamlit as st
+import datetime
+from get_and_upload_daily_production import production_class
+from debit_credit_class import debit_credit
+    
+def main():
+  st.set_page_config(page_title="Balance Sheet", page_icon=":robot:")
+  st.header("Balance Sheet")
+
+  if st.button("Insert Debit"):
+    
+  
+    
+        
+    
+        
