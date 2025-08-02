7ac8aff78f5e606d570aa2ad182aad5c17b571a1
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-08-02 09:49:30 -0700

    Update feed_app.py

--- feed_app.py
+++ feed_app.py
@@ -55,6 +55,7 @@ def main():
                 bilti_payment=cols[0].number_input("Bilti payment: ", key=f"bilti_{i}")
                 paid_by=cols[1].selectbox("Paid by: ", ['Shahid', 'Siddiq'], key=f"paidby_{i}")
                 dispatch_receipt=cols[2].text_input("dispatch receipt: ", key=f"dispatch_{i}")
+                st.write(products)
                 for j, k in enumerate(products):
                     bori_amount=cols[3+j].number_input(f"amount (bori) {k}: ", key=f"bori_{i}_{j}")
                     bori_dict[k]: bori_amount
