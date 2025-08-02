bd5f60ac2edebacff5d066d1ccfae36c078ca62f
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-08-02 11:11:05 -0700

    Update feed_app.py

--- feed_app.py
+++ feed_app.py
@@ -188,6 +188,7 @@ def main():
                     elif st.session_state.updates==2: # update for mudasir feed
                         obj=feed_class(input_dict)
                         obj.update_feed_table()
+                        st.write(input_dict['feed_bifurcation'])
                         obj1=debit_credit(input_dict)
                         obj1.update_credit_triggered_by_mudasir_log()
                         df=obj.fetch_feed_log()
