4c4911d9c18f3c68cda27e84ea84f6d0731a990a
usmanhasan123 <60095262+usmanhasan123@users.noreply.github.com>
2025-08-02 10:55:01 -0700

    Update feed_app.py

--- feed_app.py
+++ feed_app.py
@@ -190,7 +190,7 @@ def main():
                         obj1=debit_credit(input_dict)
                         # obj.update_feed_table()
                         obj1.update_credit_triggered_by_mudasir_log()
-                        df=obj.fetch_feed_log()
+                        # df=obj.fetch_feed_log()
                     elif st.session_state.updates==3: # update for masterfeed
                         obj=feed_class(input_dict)
                         obj.update_feed_table_master()
